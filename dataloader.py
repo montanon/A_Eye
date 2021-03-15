4135f2a31d5645ed3ddf390a14e4d0c1543ed7b2
montanon <montagna.p.92@hotmail.com>
2021-03-15 12:25:39 -0300

    Added dataloader

---
+++ dataloader.py
@@ -0,0 +1,103 @@
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import random
+
+class Rescale(object):
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image = sample['image']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = transform.resize(image, (new_h, new_w))
+
+        return {'image': img}
+
+class ToTensor(object):
+
+    def __call__(self, sample):
+        image = sample['image']
+
+        image = image.transpose((2, 0, 1))
+
+        return {'image': torch.from_numpy(image)}    
+
+class RGBImageDataset(Dataset):
+
+    def __init__(self, csv_file, root_dir, transform=None):
+
+        self.root_dir = root_dir
+
+        self.imgs_df = pd.read_csv(os.path.join(self.root_dir, csv_file), index_col=0)
+
+        self.transform = transform
+
+    def __getitem__(self, idx):
+
+        img_name = self.imgs_df['File'].iloc[idx]
+
+        image = io.imread(img_name)
+
+        sample = {'image': image}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def __len__(self):
+        return len(self.imgs_df)
+
+    def show_image(self, image):
+        plt.imshow(image)
+        plt.pause(0.001)
+
+    def show_sample_images(self):
+
+        # TODO: Se puede mejorar el formato del plot
+
+        fig = plt.figure()
+
+        for n_sample, i in enumerate(self.imgs_df.sample(n=4).index):
+            sample = self[i]
+
+            print(i, sample['image'].shape)
+
+            ax = plt.subplot(1, 4, n_sample + 1)
+            plt.tight_layout()
+            ax.set_title('Sample #{}'.format(i))
+            ax.axis('off')
+            self.show_image(**sample)
+
+        plt.show()
+
+
+if __name__ == '__main__':
+
+    img_transform = transforms.Compose([Rescale(100)])
+    img_dataset = RGBImageDataset(csv_file='fotos_idx.csv', root_dir='./data', transform=img_transform)
+
+    img_dataset.show_sample_images()
+
+    
+
+        
