e5d4a5689ea02d5c5e44914ab55fdb72de2563f2
montanon <montagna.p.92@hotmail.com>
2021-03-15 12:26:20 -0300

    Working in PyTorch

--- encoder.py
+++ encoder.py
@@ -5,13 +5,19 @@ from time import time
 import argparse
 import configparser
 import ast
+import numpy as np
+import pandas as pd
 
 import h5py
 
 import torch
-from torch.nn import Dropout, Linear, Module
+from torch.nn import Dropout, Flatten, Linear, Module, Sequential
 from torch.nn import Conv3d, MaxPool3d, MaxUnpool3d
 from torch.nn import Conv2d, MaxPool2d, MaxUnpool2d
+
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
 
 import matplotlib.pyplot as plt
 
@@ -43,10 +49,10 @@ class Encoder3D(Module):
 
     def __init__(self, cfg):
 
+
         super(Encoder3D, self).__init__()
 
         self.args = ConfigParse(cfg)
-
 
         self.conv3d_in_channels = ast.literal_eval(self.args['conv3d_in_channels'])
         self.conv3d_out_channels = ast.literal_eval(self.args['conv3d_out_channels'])
@@ -102,6 +108,28 @@ class Encoder3D(Module):
 
         self.dense_layers = [self.create_connected_layer(n_dense) for n_dense in range(int(self.args['n_dense']))]
 
+        self.model = self.create_sequential()
+
+    def create_sequential(self):
+
+        sequential = []
+
+        for (conv3d, dropout, max_pool) in zip(self.conv3d_layers, self.conv3d_dropout_layers, self.conv3d_pool_layers):
+
+            sequential.extend([conv3d, dropout, max_pool])
+
+        for (conv2d, dropout, max_pool) in zip(self.conv2d_layers, self.conv2d_dropout_layers, self.conv2d_pool_layers):
+
+            sequential.extend([conv2d, dropout, max_pool])
+
+        sequential.append(self.flatten)
+
+        for dense in self.dense_layers:
+
+            sequential.append(dense)
+
+        return Sequential(*sequential)
+
     def create_3d_conv_layer(self, n_layer):
 
         return Conv3d(
@@ -138,7 +166,7 @@ class Encoder3D(Module):
 
     def create_flatten_layer(self):
 
-        return torch.flatten
+        return Flatten()
                 
     def create_connected_layer(self, n_layer):
 
@@ -156,21 +184,25 @@ class Encoder3D(Module):
 
     def forward(self, x):
 
-        for conv3d_layer in self.conv3d_layers:
-            x = conv3d_layer(x)
+        output = self.model(x)
 
-        for conv2d_layer in self.conv2d_layers:
-            x = conv2d_layer
-
-        x = self.flatten(x)
-
-        for dense_layer in self.dense_layers:
-            x = dense_layer
-
-        return x
+        return output
 
 if __name__ == '__main__':
 
     EC = Encoder3D('./config/config.cfg')
 
-        
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    transform = transforms.Compose([ 
+            transforms.ToTensor(),
+            transforms.Normalize([0.0], [1.0]) 
+        ])
+
+    images = pd.read_csv('data/fotos_idx.csv', index_col=0)
+
+    
+
+    print(images.columns)
+
+    # TODO: Agregar activation
