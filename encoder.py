62fb51cc4092aa39c63ccf4f82d2b36cfb3b490f
montanon <montagna.p.92@hotmail.com>
2021-03-15 10:44:34 -0300

    Ready with PyTorch structure, missing forward

--- encoder.py
+++ encoder.py
@@ -3,15 +3,15 @@ import cv2
 import random
 from time import time
 import argparse
+import configparser
 import ast
 
 import h5py
-from keras.layers import Input, Dense, Flatten
-from keras.models import Model
-from keras.layers.convolutional import Convolution3D, MaxPooling3D, UpSampling3D
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D
-from keras.callbacks import TensorBoard
-import configparser
+
+import torch
+from torch.nn import Dropout, Linear, Module
+from torch.nn import Conv3d, MaxPool3d, MaxUnpool3d
+from torch.nn import Conv2d, MaxPool2d, MaxUnpool2d
 
 import matplotlib.pyplot as plt
 
@@ -39,7 +39,7 @@ def ConfigParse(cfg_file):
 
     return args
 
-class Encoder3D(Model):
+class Encoder3D(Module):
 
     def __init__(self, cfg):
 
@@ -47,19 +47,20 @@ class Encoder3D(Model):
 
         self.args = ConfigParse(cfg)
 
-        self.image = Input(
-                shape=(
-                        int(self.args['image_width']), 
-                        int(self.args['image_height']),
-                        3,
-                        1
-                    )
-            )
 
-        self.conv3d_features = ast.literal_eval(self.args['conv3d_features'])
+        self.conv3d_in_channels = ast.literal_eval(self.args['conv3d_in_channels'])
+        self.conv3d_out_channels = ast.literal_eval(self.args['conv3d_out_channels'])
+
         self.conv3d_kernels = ast.literal_eval(self.args['conv3d_kernels'])
 
-        self.conv3d_layers = [self.create_3d_conv_layer(n_layer) for n_layer in range(int(self.args['n_convs3d']))]    
+        self.conv3d_layers = [self.create_3d_conv_layer(n_layer) for n_layer in range(int(self.args['n_convs3d']))]   
+
+        self.conv3d_dropout = True if self.args['conv3d_dropout'] == 'true' else False
+
+        if self.conv3d_dropout:
+            self.conv3d_dropout_layers = [self.create_dropout_layer() for n_layer in range(int(self.args['n_convs3d']))]
+        else:
+            self.conv3d_dropout_layers = []
 
         self.conv3d_max_pool = True if self.args['conv3d_max_pool'] == 'true' else False
 
@@ -67,12 +68,22 @@ class Encoder3D(Model):
             self.conv3d_pool_kernels = ast.literal_eval(self.args['conv3d_pool_kernels'])
             self.conv3d_pool_layers = [self.create_3d_pool_layer(n_layer) for n_layer in range(int(self.args['n_convs3d']))]  
         else:
-            self.conv3d_pool_layers = []      
+            self.conv3d_pool_layers = []  
 
-        self.conv2d_features = ast.literal_eval(self.args['conv2d_features'])
+
+        self.conv2d_in_channels = ast.literal_eval(self.args['conv2d_in_channels'])
+        self.conv2d_out_channels = ast.literal_eval(self.args['conv2d_out_channels'])
+
         self.conv2d_kernels = ast.literal_eval(self.args['conv2d_kernels'])
 
         self.conv2d_layers = [self.create_2d_conv_layer(n_layer) for n_layer in range(int(self.args['n_convs2d']))]
+
+        self.conv2d_dropout = True if self.args['conv2d_dropout'] == 'true' else False
+
+        if self.conv2d_dropout:
+            self.conv2d_dropout_layers = [self.create_dropout_layer() for n_layer in range(int(self.args['n_convs2d']))]
+        else:
+            self.conv2d_dropout_layers = []
 
         self.conv2d_max_pool = True if self.args['conv2d_max_pool'] == 'true' else False
 
@@ -82,101 +93,81 @@ class Encoder3D(Model):
         else:
             self.conv2d_pool_layers = []   
 
+
         self.flatten = self.create_flatten_layer() 
 
-        self.dense_features = ast.literal_eval(self.args['dense_features'])
 
-        self.dense_layers = [self.create_dense_layer(n_dense) for n_dense in range(int(self.args['n_dense']))]
+        self.dense_in_features = ast.literal_eval(self.args['dense_in_channels'])
+        self.dense_out_features = ast.literal_eval(self.args['dense_out_channels'])
+
+        self.dense_layers = [self.create_connected_layer(n_dense) for n_dense in range(int(self.args['n_dense']))]
 
     def create_3d_conv_layer(self, n_layer):
 
-        return Convolution3D(
-                self.conv3d_features[n_layer], 
-                self.conv3d_kernels[n_layer],
-                activation=self.args['activation'],
-                padding=self.args['padding'],
-                use_bias=True,
-                kernel_initializer="glorot_uniform",
-                bias_initializer="zeros",
-                kernel_regularizer=None,
-                bias_regularizer=None,
-                activity_regularizer=None,
-                kernel_constraint=None,
-                bias_constraint=None
+        return Conv3d(
+                in_channels=self.conv3d_in_channels[n_layer],
+                out_channels=self.conv3d_out_channels[n_layer],
+                kernel_size=self.conv3d_kernels[n_layer],
+                padding=int(self.args['padding']),
+                bias=True              
             )
                     
     def create_3d_pool_layer(self, n_layer):
 
-        return MaxPooling3D(
-                pool_size=self.conv3d_pool_kernels[n_layer],
-                strides=None,
-                padding=self.args['padding'],
-                data_format=None
+        return MaxPool3d(
+                kernel_size=self.conv3d_pool_kernels[n_layer],
+                padding=int(self.args['padding'])
             )     
         
     def create_2d_conv_layer(self, n_layer):
 
-        return Convolution2D(
-                self.conv2d_features[n_layer],
-                self.conv2d_kernels[n_layer],
-                activation=self.args['activation'],
-                padding=self.args['padding'],
-                use_bias=True,
-                kernel_initializer="glorot_uniform",
-                bias_initializer="zeros",
-                kernel_regularizer=None,
-                bias_regularizer=None,
-                activity_regularizer=None,
-                kernel_constraint=None,
-                bias_constraint=None
-            )  
+        return Conv2d(
+                in_channels=self.conv2d_in_channels[n_layer],
+                out_channels=self.conv2d_out_channels[n_layer],
+                kernel_size=self.conv2d_kernels[n_layer],
+                padding=int(self.args['padding']),
+                bias=True              
+            )
 
     def create_2d_pool_layer(self, n_layer):
 
-        return MaxPooling2D(
-                pool_size=self.conv2d_pool_kernels[n_layer],
-                strides=None,
-                padding=self.args['padding'],
-                data_format=None
+        return MaxPool2d(
+                kernel_size=self.conv2d_pool_kernels[n_layer],
+                padding=int(self.args['padding'])
             )     
 
     def create_flatten_layer(self):
 
-        return Flatten(
-            data_format=None
-        )
+        return torch.flatten
                 
-    def create_dense_layer(self, n_dense):
+    def create_connected_layer(self, n_layer):
 
-        return Dense(
-                    self.dense_features[n_dense],
-                    activation=None,
-                    use_bias=True,
-                    kernel_initializer="glorot_uniform",
-                    bias_initializer="zeros",
-                    kernel_regularizer=None,
-                    bias_regularizer=None,
-                    activity_regularizer=None,
-                    kernel_constraint=None,
-                    bias_constraint=None
+        return Linear(
+                    in_features=self.dense_in_features[n_layer],
+                    out_features=self.dense_out_features[n_layer],
+                    bias=True
                 )
 
-        def run(self, x):
+    def create_dropout_layer(self):
 
-            x = self.input(x)
+        return Dropout(
+            p=float(self.args['dropout_p'])
+        )
 
-            for conv3d_layer in self.conv3d_layers:
-                x = conv3d_layer(x)
+    def forward(self, x):
 
-            for conv2d_layer in self.conv2d_layers:
-                x = conv2d_layer
+        for conv3d_layer in self.conv3d_layers:
+            x = conv3d_layer(x)
 
-            x = self.flatten(x)
+        for conv2d_layer in self.conv2d_layers:
+            x = conv2d_layer
 
-            for dense_layer in self.dense_layers:
-                x = dense_layer
+        x = self.flatten(x)
 
-            return x
+        for dense_layer in self.dense_layers:
+            x = dense_layer
+
+        return x
 
 if __name__ == '__main__':
 
