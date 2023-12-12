c6f2ff6c9ab5c5c37f11fbf2466747bedcd16172
montanon <montagna.p.92@hotmail.com>
2023-12-12 15:08:49 +0900

    fix: minor refactor

--- dataloader.py
+++ dataloader.py
@@ -1,24 +1,22 @@
 import os
-import torch
-import pandas as pd
-from skimage import io, transform
-import numpy as np
-import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
 import random
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from skimage import io, transform
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms, utils
+
+
 class ToCuda(object):
-
     def __call__(self, sample):
-
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
         image = sample['image'].to(device=device, dtype=torch.float)
-
         image = image.unsqueeze(0)
-
         return {'image': image}
+
 
 class Rescale(object):
 
@@ -28,7 +26,6 @@ class Rescale(object):
 
     def __call__(self, sample):
         image = sample['image']
-
         h, w = image.shape[:2]
         if isinstance(self.output_size, int):
             if h > w:
@@ -37,44 +34,32 @@ class Rescale(object):
                 new_h, new_w = self.output_size, self.output_size * 1.5
         else:
             new_h, new_w = self.output_size
-
         new_h, new_w = int(new_h), int(new_w)
-
         img = transform.resize(image, (new_h, new_w))
-
         return {'image': img}
 
+
 class ToTensor(object):
-
     def __call__(self, sample):
-
         image = sample['image']
-
         image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image)}
 
-        return {'image': torch.from_numpy(image)}    
 
 class RGBImageDataset(Dataset):
 
     def __init__(self, csv_file, root_dir, transform=None):
-
         self.root_dir = root_dir
-
-        self.imgs_df = pd.read_csv(os.path.join(self.root_dir, csv_file), index_col=0)
-
+        self.imgs_df = pd.read_csv(os.path.join(
+            self.root_dir, csv_file), index_col=0)
         self.transform = transform
 
     def __getitem__(self, idx):
-
         img_name = self.imgs_df['File'].iloc[idx]
-
         image = io.imread(img_name)
-
         sample = {'image': image}
-
         if self.transform:
             sample = self.transform(sample)
-
         return sample
 
     def __len__(self):
@@ -85,48 +70,35 @@ class RGBImageDataset(Dataset):
         plt.pause(0.001)
 
     def show_sample_images(self):
-
-        # TODO: Se puede mejorar el formato del plot
-
         fig = plt.figure()
-
         for n_sample, i in enumerate(self.imgs_df.sample(n=4).index):
             sample = self[i]
-
             print(i, sample['image'].shape)
-
             ax = plt.subplot(1, 4, n_sample + 1)
             plt.tight_layout()
             ax.set_title('Sample #{}'.format(i))
             ax.axis('off')
             self.show_image(**sample)
-
         plt.show()
 
     def show_sample_tensors(self):
-
         for n_sample, i in enumerate(self.imgs_df.sample(n=4).index):
             sample = self[i]
-
             print(i, sample['image'].size())
 
+
 if __name__ == '__main__':
-
     img_dataset = RGBImageDataset(csv_file='fotos_idx.csv',
-                                root_dir='./data'
-                                )
-
+                                  root_dir='./data'
+                                  )
     img_dataset.show_sample_images()
-
     img_transform = transforms.Compose([Rescale(100)])
-
     img_dataset = RGBImageDataset(csv_file='fotos_idx.csv',
-                                root_dir='./data',
-                                transform=transforms.Compose([
-                                    Rescale(100),
-                                    ToTensor(),
-                                    ToCuda()
-                                ])
-                                )
-
+                                  root_dir='./data',
+                                  transform=transforms.Compose([
+                                      Rescale(100),
+                                      ToTensor(),
+                                      ToCuda()
+                                  ])
+                                  )
     img_dataset.show_sample_tensors()
--- encoder.py
+++ encoder.py
@@ -1,197 +1,178 @@
+import argparse
+import ast
+import configparser
 import os
-import cv2
 import random
 from time import time
-import argparse
-import configparser
-import ast
+
+import cv2
+import h5py
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-import h5py
-
 import torch
-from torch.nn import Dropout, Flatten, Linear, Module, Sequential
-from torch.nn import Conv3d, MaxPool3d, MaxUnpool3d
-from torch.nn import Conv2d, MaxPool2d, MaxUnpool2d
-
 import torchvision
-from torch.utils.data import Dataset, DataLoader
+from torch.nn import (
+    Conv2d,
+    Conv3d,
+    Dropout,
+    Flatten,
+    Linear,
+    MaxPool2d,
+    MaxPool3d,
+    MaxUnpool2d,
+    MaxUnpool3d,
+    Module,
+    Sequential,
+)
+from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, utils
 
-from dataloader import RGBImageDataset, Rescale, ToTensor,ToCuda
+from dataloader import Rescale, RGBImageDataset, ToCuda, ToTensor
 
-import matplotlib.pyplot as plt
 
 def ConfigParse(cfg_file, model='ENCODER'):
-
     parser = configparser.ConfigParser()
-
     parser.read(cfg_file)
-
     args = parser[model]
-
     return args
+
 
 class Encoder3D(Module):
 
     def __init__(self, cfg):
-
-
         super(Encoder3D, self).__init__()
-
         self.args = ConfigParse(cfg)
-
-        self.conv3d_in_channels = ast.literal_eval(self.args['conv3d_in_channels'])
-        self.conv3d_out_channels = ast.literal_eval(self.args['conv3d_out_channels'])
-
+        self.conv3d_in_channels = ast.literal_eval(
+            self.args['conv3d_in_channels'])
+        self.conv3d_out_channels = ast.literal_eval(
+            self.args['conv3d_out_channels'])
         self.conv3d_kernels = ast.literal_eval(self.args['conv3d_kernels'])
-
-        self.conv3d_layers = [self.create_3d_conv_layer(n_layer) for n_layer in range(int(self.args['n_convs3d']))]   
-
+        self.conv3d_layers = [self.create_3d_conv_layer(
+            n_layer) for n_layer in range(int(self.args['n_convs3d']))]
         self.conv3d_dropout = True if self.args['conv3d_dropout'] == 'true' else False
 
         if self.conv3d_dropout:
-            self.conv3d_dropout_layers = [self.create_dropout_layer() for n_layer in range(int(self.args['n_convs3d']))]
+            self.conv3d_dropout_layers = [self.create_dropout_layer(
+            ) for n_layer in range(int(self.args['n_convs3d']))]
         else:
             self.conv3d_dropout_layers = []
 
         self.conv3d_max_pool = True if self.args['conv3d_max_pool'] == 'true' else False
 
         if self.conv3d_max_pool:
-            self.conv3d_pool_kernels = ast.literal_eval(self.args['conv3d_pool_kernels'])
-            self.conv3d_pool_layers = [self.create_3d_pool_layer(n_layer) for n_layer in range(int(self.args['n_convs3d']))]  
+            self.conv3d_pool_kernels = ast.literal_eval(
+                self.args['conv3d_pool_kernels'])
+            self.conv3d_pool_layers = [self.create_3d_pool_layer(
+                n_layer) for n_layer in range(int(self.args['n_convs3d']))]
         else:
-            self.conv3d_pool_layers = []  
+            self.conv3d_pool_layers = []
 
-
-        self.conv2d_in_channels = ast.literal_eval(self.args['conv2d_in_channels'])
-        self.conv2d_out_channels = ast.literal_eval(self.args['conv2d_out_channels'])
-
+        self.conv2d_in_channels = ast.literal_eval(
+            self.args['conv2d_in_channels'])
+        self.conv2d_out_channels = ast.literal_eval(
+            self.args['conv2d_out_channels'])
         self.conv2d_kernels = ast.literal_eval(self.args['conv2d_kernels'])
-
-        self.conv2d_layers = [self.create_2d_conv_layer(n_layer) for n_layer in range(int(self.args['n_convs2d']))]
-
+        self.conv2d_layers = [self.create_2d_conv_layer(
+            n_layer) for n_layer in range(int(self.args['n_convs2d']))]
         self.conv2d_dropout = True if self.args['conv2d_dropout'] == 'true' else False
 
         if self.conv2d_dropout:
-            self.conv2d_dropout_layers = [self.create_dropout_layer() for n_layer in range(int(self.args['n_convs2d']))]
+            self.conv2d_dropout_layers = [self.create_dropout_layer(
+            ) for n_layer in range(int(self.args['n_convs2d']))]
         else:
             self.conv2d_dropout_layers = []
 
         self.conv2d_max_pool = True if self.args['conv2d_max_pool'] == 'true' else False
 
         if self.conv2d_max_pool:
-            self.conv2d_pool_kernels = ast.literal_eval(self.args['conv2d_pool_kernels'])
-            self.conv2d_pool_layers = [self.create_2d_pool_layer(n_layer) for n_layer in range(int(self.args['n_convs2d']))]  
+            self.conv2d_pool_kernels = ast.literal_eval(
+                self.args['conv2d_pool_kernels'])
+            self.conv2d_pool_layers = [self.create_2d_pool_layer(
+                n_layer) for n_layer in range(int(self.args['n_convs2d']))]
         else:
-            self.conv2d_pool_layers = []   
+            self.conv2d_pool_layers = []
 
         self.model = self.create_sequential().cuda()
 
     def create_sequential(self):
-
         sequential = []
-
         for (conv3d, dropout, max_pool) in zip(self.conv3d_layers, self.conv3d_dropout_layers, self.conv3d_pool_layers):
-
             sequential.extend([conv3d, dropout, max_pool])
-
         for (conv2d, dropout, max_pool) in zip(self.conv2d_layers, self.conv2d_dropout_layers, self.conv2d_pool_layers):
-
             sequential.extend([conv2d, dropout, max_pool])
-
         return Sequential(*sequential)
 
     def create_3d_conv_layer(self, n_layer):
-
         return Conv3d(
-                in_channels=self.conv3d_in_channels[n_layer],
-                out_channels=self.conv3d_out_channels[n_layer],
-                kernel_size=self.conv3d_kernels[n_layer],
-                padding=int(self.args['padding']),
-                stride=int(self.args['stride']),
-                bias=True              
-            )
-                    
+            in_channels=self.conv3d_in_channels[n_layer],
+            out_channels=self.conv3d_out_channels[n_layer],
+            kernel_size=self.conv3d_kernels[n_layer],
+            padding=int(self.args['padding']),
+            stride=int(self.args['stride']),
+            bias=True
+        )
+
     def create_3d_pool_layer(self, n_layer):
-
         return MaxPool3d(
-                kernel_size=self.conv3d_pool_kernels[n_layer],
-                padding=int(self.args['padding']),
-                return_indices=True
-            )     
-        
-    def create_2d_conv_layer(self, n_layer):
+            kernel_size=self.conv3d_pool_kernels[n_layer],
+            padding=int(self.args['padding']),
+            return_indices=True
+        )
 
+    def create_2d_conv_layer(self, n_layer):
         return Conv2d(
-                in_channels=self.conv2d_in_channels[n_layer],
-                out_channels=self.conv2d_out_channels[n_layer],
-                kernel_size=self.conv2d_kernels[n_layer],
-                padding=int(self.args['padding']),
-                bias=True              
-            )
+            in_channels=self.conv2d_in_channels[n_layer],
+            out_channels=self.conv2d_out_channels[n_layer],
+            kernel_size=self.conv2d_kernels[n_layer],
+            padding=int(self.args['padding']),
+            bias=True
+        )
 
     def create_2d_pool_layer(self, n_layer):
-
         return MaxPool2d(
-                kernel_size=self.conv2d_pool_kernels[n_layer],
-                padding=int(self.args['padding']),
-                return_indices=True
-            )     
+            kernel_size=self.conv2d_pool_kernels[n_layer],
+            padding=int(self.args['padding']),
+            return_indices=True
+        )
 
     def create_flatten_layer(self):
-
         return Flatten()
-                
-    def create_connected_layer(self, n_layer):
 
+    def create_connected_layer(self, n_layer):
         return Linear(
-                    in_features=self.dense_in_features[n_layer],
-                    out_features=self.dense_out_features[n_layer],
-                    bias=True
-                )
+            in_features=self.dense_in_features[n_layer],
+            out_features=self.dense_out_features[n_layer],
+            bias=True
+        )
 
     def create_dropout_layer(self):
-
         return Dropout(
             p=float(self.args['dropout_p'])
         )
 
     def forward(self, x):
-
         idxs = []
-
         for layer in self.model:
-
             if isinstance(layer, MaxPool2d) or isinstance(layer, MaxPool3d):
                 x, idx = layer(x)
                 idxs.append(idx)
-
             else:
                 x = layer(x)
-
         return x, idxs
 
+
 if __name__ == '__main__':
-
     EC = Encoder3D('./config/config.cfg')
-
     img_dataset = RGBImageDataset(csv_file='fotos_idx.csv',
-                                root_dir='./data',
-                                transform=transforms.Compose([
-                                    Rescale(100),
-                                    ToTensor(),
-                                    ToCuda()
-                                ])
-                                )
-
-    #img_dataset.show_sample_tensors()
-
+                                  root_dir='./data',
+                                  transform=transforms.Compose([
+                                      Rescale(100),
+                                      ToTensor(),
+                                      ToCuda()
+                                  ])
+                                  )
+    img_dataset.show_sample_tensors()
     x = img_dataset[0]
-
     output, pool_idxs = EC(x['image'])
-
     print(output.shape)
-
-    # TODO: Agregar activation
